30bec5c7f45683a1b8399950c9e3c1e5f6bf08ef
julianbordet <bordet.julian@gmail.com>
2023-06-25 16:15:53 -0300

    added axios basic get request

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../Components/Card'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+
+  const [dentistas, setDentistas] = useState([]);
+
+  const getDentists = async() => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    setDentistas(response.data);
+  }
+
+  useEffect(() =>{
+    getDentists();
+  }, [])
+
   return (
     <main className="" >
       <Navbar/>
       <h1>Home</h1>
-      <h1>HOME PAGEE</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
+        {dentistas && console.log(dentistas)}
       </div>
       <Footer />
     </main>
