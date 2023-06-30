afeeee68bd26c270a9ac61a81f763b8e25deec77
julianbordet <bordet.julian@gmail.com>
2023-06-30 12:06:29 -0300

    more changes to theming

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -3,10 +3,13 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import { useEffect, useState } from "react";
 import Card from "../Components/Card";
+import { useGlobalContext } from "../Components/utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+
+  const { state, dispatch } = useGlobalContext();
 
   const [favDentists, setfavDentists] = useState([]);
   
@@ -19,16 +22,16 @@ const Favs = () => {
   
 
   return (
-    <>
-      <Navbar />
-      <h1>Dentists Favs</h1>
-      <div className="card-grid">
-        {favDentists.length > 0 && favDentists.map(dentista => <Card key={dentista.id} dentista={dentista} />)}
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
-      </div>
-      <Footer />
-    </>
+      <section className={state.theme === 'dark' ? 'routesDark' : 'routesLight'}>
+        <Navbar />
+          <h1 className={state.theme === 'dark' ? 'typographyDark' : 'typographyLight'}>Dentists Favs</h1>
+          <div className="card-grid">
+            {favDentists.length > 0 && favDentists.map(dentista => <Card key={dentista.id} dentista={dentista} />)}
+            {/* este componente debe consumir los destacados del localStorage */}
+            {/* Deberan renderizar una Card por cada uno de ellos */}
+          </div>
+        <Footer />
+      </section>
   );
 };
 
